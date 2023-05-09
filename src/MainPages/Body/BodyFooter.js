47d003ca376a115d317b6b4dd213943dd0e3ff6f
deveshps <deveshps@dewsolutions.in>
2023-05-09 11:42:54 +0530

    share file changes

--- src/MainPages/Body/BodyFooter.js
+++ src/MainPages/Body/BodyFooter.js
@@ -4,7 +4,7 @@ import HeartIcon from "../../images/heart-icon.png"
 import ShareIcon from "../../images/share-icon.png"
 
 const BodyFooter = ({data}) => {
-    const handleShare = async () => {
+    const handleShares = async () => {
         const item = {
             name: "T042323-1355-L",
             "date": "04-23-2023",
@@ -98,6 +98,119 @@ const BodyFooter = ({data}) => {
             console.error(error)
           }
     }
+    const handleShare = async (title) => {
+         const item = {
+            name: "T042323-1355-L",
+            "date": "04-23-2023",
+            "time": "01.55PM",
+            data: [
+                {
+                    "time": 0,
+                    "temp": 0
+                },
+                {
+                    "time": 1,
+                    "temp": 8.1
+                },
+                {
+                    "time": 2,
+                    "temp": 16.2
+                },
+                {
+                    "time": 3,
+                    "temp": 24.2
+                },
+                {
+                    "time": 4,
+                    "temp": 32
+                },
+                {
+                    "time": 5,
+                    "temp": 39.5
+                },
+                {
+                    "time": 6,
+                    "temp": 46.6
+                },
+                {
+                    "time": 7,
+                    "temp": 53.4
+                },
+                {
+                    "time": 8,
+                    "temp": 59.8
+                },
+                {
+                    "time": 9,
+                    "temp": 65.6
+                },
+                {
+                    "time": 10,
+                    "temp": 71
+                },
+                {
+                    "time": 11,
+                    "temp": 75.7
+                }
+            ]
+        }
+        const selectedButton="temperature";
+    if(item){
+      if (!navigator.canShare) {
+        console.log("Your browser doesn't support the Web Share API.")
+        return;
+      }
+      try {
+        let header = ["Time ( Sec )", "Temperature ( C )"];
+        if (selectedButton === "voltage") header = ["Time ( Sec )", "Voltage (V)"];
+        else if (selectedButton === "rgb")
+          header = ["Measurement No.", "RED", "GREEN", "BLUE"];
+        
+        let csv = "";
+        if(header && header[2] === "GREEN" && item?.isCalibratedAndTested){
+          csv += "Calibrated and Tested";
+          csv += "\n";
+        }
+        if (header) {
+          for (let one of header) {
+            csv += one + ",";
+          }
+          csv += "\n";
+        }
+        // csv += data.name + '\n';
+        if (item && item.data && item.data.length > 0) {
+          for(let one of item.data){
+            if(header && header[1] === "Temperature ( C )"){
+              csv += one.time;
+              csv += "," + one.temp;
+            }else if(header && header[1] === "Voltage (V)"){
+              csv += one.time;
+              csv += "," + one.voltage;
+            }else if(header && header[2] === "GREEN"){
+              csv += one["Measuement No"];
+              csv += "," + one['RED'];
+              csv += "," + one['GREEN'];
+              csv += "," + one['BLUE'];
+      
+            }
+            csv += "\n";
+          }
+        }
+        const file = new File([csv], `${item?.name}.csv`,{type:"text/csv"});
+        // console.log("???????? ",file)
+        await navigator.share({
+          url:`${selectedButton} Experiment data of ${item.name}`,
+          text: `${selectedButton} data of ${item?.name}`,
+          title: `${selectedButton} Experiment Data`,
+          files:[file]
+        });
+        console.log("data has been shared Successfully!")
+      } catch (error) {
+        console.error(error)
+      }
+      // console.log("share data",item)
+    }
+  };
     return <div className="bodyFooter">
     <div className="bodyFooterSubWrapper">
         <img src={CommentIcon} style={{width:15}} alt="comment"/>
