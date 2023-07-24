14452f35a746e62b386e0a489e97973f699fcbc1
deveshps <deveshps@dewsolutions.in>
2023-07-24 18:32:10 +0530

    chnages

--- src/FirstTimePage.js
+++ src/FirstTimePage.js
@@ -3,6 +3,7 @@ import {useNavigate} from "react-router"
 import './FirstTimePage.css';
 import FirstTimeLogo from "./images/first-login.png"
 import RezolveLogo from "./images/rezolve-logo.png"
+import TemperatureGraph from "./MainPages/Body/GraphPage";
 
 const FirstTimePage = () => {
     const navigate = useNavigate();
@@ -13,6 +14,7 @@ const FirstTimePage = () => {
         <div className="headeText">First Time User - Login By Phone Number</div>
         <img src={FirstTimeLogo} onClick={submitHander} className="proceedImage" alt="FirstTimeLogo"/>
         <img src={RezolveLogo} alt = "RezolveLogo" className="footer" />
+        <TemperatureGraph />
     </div>
 }
 export default FirstTimePage
